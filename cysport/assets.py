4b0962e51e3b9aabb948fd645de0b81068cc2cd1
ZOE <zooey.peng@gmail.com>
2013-09-21 00:32:21 +0800

    a-line

--- cysport/assets.py
+++ cysport/assets.py
@@ -22,6 +22,12 @@ css_index = Bundle(
     output='site/css/index.css',
 )
 
+css_a_line = Bundle(
+    'site/scss/a-line.scss',
+    filters='scss',
+    output='site/css/a-line.css',
+)
+
 def register_assets(app):
     """ Register Assets to the app """
 
@@ -33,6 +39,7 @@ def register_assets(app):
     assets.register('css_common', css_common)
     assets.register('css_base', css_base)
     assets.register('css_index', css_index)
+    assets.register('css_a_line', css_a_line)
 
     # Init App
     assets.init_app(app)
--- cysport/views.py
+++ cysport/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/a-line')
+def a_line():
+    return render_template('a-line.html')
